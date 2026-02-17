135634f8956dfac604a735ee9dc65c670564f735
Tempest <70986132+KingTempest07@users.noreply.github.com>
2026-02-17 12:57:15 -0600

    Add ceiling and target player mouse on paddle hit

--- scenes/Ball.gd
+++ scenes/Ball.gd
@@ -3,6 +3,7 @@ class_name  Ball
 
 const BALL = 4
 const BLOCK = 8
+const PLAYER = 2
 
 # We will set the ball in motion and let the 2D physics system do the rest.
 # See the README for settings to make this work correctly.
@@ -10,20 +11,23 @@ const BLOCK = 8
 @export var initial_velocity:= 850.0
 @export var velocity_increase_on_bounce:= 25.0
 @export var max_velocity:= 1000.0
+var current_velocity: float
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready() -> void:
+	current_velocity = initial_velocity
 	var rand_angle:= randf_range(3*PI/4, PI/4)
 	var direction:= Vector2.from_angle(rand_angle)
-	apply_force(direction * initial_velocity)
+	linear_velocity = direction * current_velocity
 	
 	
 func _process(delta: float) -> void:
 	# squared for perf (doesnt matter ig, but i like to do it anyways lol)
 	if linear_velocity.length_squared() > max_velocity ** 2:
 		linear_velocity = linear_velocity.normalized() * max_velocity
+		current_velocity = max_velocity
 
 
 
@@ -32,4 +36,8 @@ func _on_body_exited(body: Node) -> void:
 	var layer = body.collision_layer
 	if layer == BLOCK:
 		body.queue_free()
-		linear_velocity += linear_velocity.normalized() * velocity_increase_on_bounce
+		current_velocity += velocity_increase_on_bounce
+		linear_velocity = linear_velocity.normalized() * current_velocity
+	if layer == PLAYER:
+		var dir_to_mouse:= (get_global_mouse_position() - global_position).normalized()
+		linear_velocity = dir_to_mouse * current_velocity

a6bac3dfb7bd4474f26fe5819e391cf0ec54e4cf
Tempest <70986132+KingTempest07@users.noreply.github.com>
2026-02-17 12:48:20 -0600

    Ball max velocity, rand angle, speed up on block hit
    Also lock rotation of ball

--- scenes/Ball.gd
+++ scenes/Ball.gd
@@ -7,13 +7,23 @@ const BLOCK = 8
 # We will set the ball in motion and let the 2D physics system do the rest.
 # See the README for settings to make this work correctly.
 
-@export var initial_velocity = Vector2(700,500)
+@export var initial_velocity:= 850.0
+@export var velocity_increase_on_bounce:= 25.0
+@export var max_velocity:= 1000.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _ready() -> void:
-	apply_force(initial_velocity)
+	var rand_angle:= randf_range(3*PI/4, PI/4)
+	var direction:= Vector2.from_angle(rand_angle)
+	apply_force(direction * initial_velocity)
+	
+	
+func _process(delta: float) -> void:
+	# squared for perf (doesnt matter ig, but i like to do it anyways lol)
+	if linear_velocity.length_squared() > max_velocity ** 2:
+		linear_velocity = linear_velocity.normalized() * max_velocity
 
 
 
@@ -22,4 +32,4 @@ func _on_body_exited(body: Node) -> void:
 	var layer = body.collision_layer
 	if layer == BLOCK:
 		body.queue_free()
-		
+		linear_velocity += linear_velocity.normalized() * velocity_increase_on_bounce
